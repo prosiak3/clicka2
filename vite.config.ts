4582fc14b580e7aecfdd5bc35705ba2905f3434b
prosiak3 <57153435+prosiak3@users.noreply.github.com>
2025-10-31 22:23:50 +0100

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon-*.png', 'icon-*.svg', 'apple-touch-icon.svg'],
+      includeAssets: ['icon-*.png', 'icon-*.svg', 'apple-touch-icon.png'],
       manifest: {
         name: 'Clicka - Better Fishing',
         short_name: 'Clicka',
